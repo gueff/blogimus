b4685f23422904d5ab4c17ceb4e5ddf840916d93
Guido K.B.W. Üffing <gueff@users.noreply.github.com>
2018-04-02 21:47:01 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ cp -r ./_INSTALL/data				./
 echo "...done!";
 
 echo "installing libraries...";
-php $MVC_APPLICATION_PATH/composer.phar --working-dir=../../config/Blogixx/ install;
+php $MVC_APPLICATION_PATH/composer.phar --working-dir=../../config/Blogimus/ install;
 echo "...done!";
 
 
