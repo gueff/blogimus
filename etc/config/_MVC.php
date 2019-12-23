b3474cf8e0d21cdf131f5168f4e68e7f54e7c456
Guido K.B.W. Üffing <gueff@users.noreply.github.com>
2019-12-23 22:19:35 +0100

    Update _MVC.php

--- etc/config/_MVC.php
+++ etc/config/_MVC.php
@@ -10,12 +10,12 @@ $aConfig['MVC_DEBUG'] = false;
  * Misc
  */
 // override MVC's fallback routing
-$aConfig['MVC_ROUTING_FALLBACK'] = $aConfig['MVC_GET_PARAM_MODULE'] . '=Webbixx&'
+$aConfig['MVC_ROUTING_FALLBACK'] = $aConfig['MVC_GET_PARAM_MODULE'] . '=Blogimus&'
     . $aConfig['MVC_GET_PARAM_C'] . '=index&'
     . $aConfig['MVC_GET_PARAM_M'] . '=index';
 
 // composer dir
-$aConfig['MVC_COMPOSER_DIR'] = realpath(__DIR__ . '/../') . '/config/Webbixx';
+$aConfig['MVC_COMPOSER_DIR'] = realpath(__DIR__ . '/../') . '/config/Blogimus';
 
 // routing.json file
 $aConfig['MVC_ROUTING_JSON'] = realpath(__DIR__ . '/../') . '/routing/final.json';
